9a1359a965e8d6b16b4a6ac19716ba2ecb2fc64a
Blaxsz <joaovictor.gizidoro@pm.me>
2023-11-02 17:22:56 -0300

    add dwm, dwmblocks and dmenu

---
+++ suckless/.src/dwm/themes/catppuccin-latte.h
@@ -0,0 +1,6 @@
+static char col_base00[] = "#eff1f5";
+static char col_base01[] = "#bcc0cc";
+static char col_base02[] = "#4c4f69";
+static char col_base04[] = "#7287fd";
+static char col_base0C[] = "#209fb5";
+static char col_base0D[] = "#acb0be";
---
+++ suckless/.src/dwm/themes/one-light.h
@@ -0,0 +1,8 @@
+static const char col_base00[]      = "#fafafa";
+static const char col_base01[]      = "#f0f0f1";
+static const char col_base02[]      = "#e5e5e6";
+static const char col_base04[]      = "#696c77";
+static const char col_base0C[]      = "#0184bc";
+static const char col_base0D[]      = "#4078f2";
+
+
